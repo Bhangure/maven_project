43f6ab6c779d232ee9ded9e2d06d24c0b2750112
Bhangure <112766432+Bhangure@users.noreply.github.com>
2023-02-14 08:28:04 +0530

    updated

--- src/test/java/com/springexample/sample/SampleApplicationTests.java
+++ src/test/java/com/springexample/sample/SampleApplicationTests.java
@@ -3,6 +3,9 @@ package com.springexample.sample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+
 @SpringBootTest
 class SampleApplicationTests {
 
